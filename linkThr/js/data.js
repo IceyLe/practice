a482e82513eeb1ebb36443885f7ceb6bf1706ab7
le <sunledream@163.com>
2015-01-28 17:11:12 +0800

    create a three-level linkage

---
+++ linkThr/js/data.js
@@ -0,0 +1,33 @@
+var ProvinceLink={
+    'bj' :{
+    	name : '北京',
+    	city : {
+            'bjs':{
+                name : '北京市',
+                district : [
+                    {name :'昌平区'},
+                    {name :'海淀区'}
+                ]
+            }
+    	}
+    },
+    'hb' :{
+    	name :'河北省',
+    	city :{
+    		'sjz':{
+                name :'石家庄市',
+                district :[
+                    {name :'桥东区'},
+                    {name :'长安区'}
+                ]
+            },
+            'qhd':{
+                name :'秦皇岛市',
+                district :[
+                    { name :'海港区'},
+                    { name :'海港区'}
+                ]
+            }
+    	}
+    }
+};
---
+++ linkThr/js/page.js
@@ -0,0 +1,10 @@
+(function(){
+    // onloadHead();
+
+
+    var niceLink = new LinkStr("threelink","pro","cit","dis");
+    niceLink.init();
+
+    var niceLink1 = new LinkStr("threelink1","pro1","cit1","dis1");
+    niceLink1.init();
+})();
